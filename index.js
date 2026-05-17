c6a1ea17cc3adf129dc41a47da5bf530ff47b730
dulal71 <ahmeddulal4211@gmail.com>
2026-05-18 00:20:33 +0600

    add api for get all dada

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,6 +7,44 @@ const port =process.env.PORT || 5000
 dotenv.config()
 app.use(express.json())
 app.use(cors())
+const uri=process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+const db=client.db('doctor-appoint-data')
+const doctors=db.collection('doctors')
+const bookedDoctor = db.collection("doctorBookings")
+   
+//get all data 
+app.get('/doctors',async(req,res)=>{
+  const cursor=await doctors.find()
+  const result = await cursor.toArray()
+  res.send(result)
+})
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
